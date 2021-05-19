67f1c4f4fb182b94686c14e5500917ee4d2751b9
andy-s-byte <soupianas.andreas@nbg.gr>
2021-05-19 09:33:57 +0300

    Initial commit

---
+++ src/main/java/nbg/gr/simple/SimpleApp.java
@@ -0,0 +1,13 @@
+package nbg.gr.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApp.class, args);
+    }
+
+}
